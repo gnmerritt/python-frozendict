d684b189f9e0a61b0a54f4e6944c650eb42ac621
Nathan Merritt <gnm@the-merritts.net>
2020-10-15 17:32:42 -0600

    update for modern python

--- frozendict/__init__.py
+++ frozendict/__init__.py
@@ -1,19 +1,11 @@
 import collections
-import operator
-import functools
-import sys
+from collections import OrderedDict
 
 
-try:
-    from collections import OrderedDict
-except ImportError:  # python < 2.7
-    OrderedDict = NotImplemented
+iteritems = getattr(dict, 'iteritems', dict.items)  # py2-3 compatibility
 
 
-iteritems = getattr(dict, 'iteritems', dict.items) # py2-3 compatibility
-
-
-class frozendict(collections.Mapping):
+class frozendict(collections.abc.Mapping):
     """
     An immutable wrapper around dictionaries that implements the complete :py:class:`collections.Mapping`
     interface. It can be used as a drop-in replacement for dictionaries where immutability is desired.
